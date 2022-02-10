9e287541480be38bf2458fae527ea9388e98bbe5
Amirhossein Mojiri Foroushani <amojiry@gmail.com>
2022-02-10 16:33:39 +0200

    Create seeders and factories

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ */
+class TodoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->text(),
+            'user_id' => User::factory(),
+            'status' => $this->faker->randomElement(['NotStarted', 'OnGoing', 'Completed']),
+        ];
+    }
+}
